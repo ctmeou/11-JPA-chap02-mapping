fcbb0d0dd1885c045069d481016ff7240003f53b
ctmeou <ictmeousz@gmail.com>
2023-10-23 17:25:22 +0900

    10/23 himedia 11.JPA - 02.mapping

---
+++ src/test/java/com/ohgiraffers/section01/entity/EntityMappingTests.java
@@ -0,0 +1,63 @@
+package com.ohgiraffers.section01.entity;
+
+import org.junit.jupiter.api.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EntityMappingTests {
+
+    //application 당 1개만
+    private static EntityManagerFactory entityManagerFactory;
+
+    //스레드 세이프 하지 않고, 요청 당 1개
+    private EntityManager entityManager;
+
+    @BeforeAll //junit에서 오는 어노테이션, 테스트가 진행되기 전에 한 번 진행된다.
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach //테스트 하나가 진행되기 전에 한 번씩
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll //테스트가 끝나기 전에 한 번만
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach //테스트가 끝날 떄마다 한 번씩
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @Test
+    public void 테이블_만들기_테스트() {
+
+        // given
+        Member member = new Member();
+        member.setMemberNo(1);
+        member.setMemberId("user01");
+        member.setMemberPwd("pass01");
+        member.setNickname("홍길동");
+        member.setPhone("010-1234-5678");
+        member.setAddress("서울시 종로구");
+        member.setEnrollDate(new Date());
+        member.setMemberRole("ROLE_MEMBER");
+        member.setStatus("Y");
+
+        // when
+        entityManager.persist(member);
+
+        // then
+        Member foundMember = entityManager.find(Member.class, member.getMemberNo());
+        assertEquals(member.getMemberNo(), foundMember.getMemberNo());
+
+    }
+}
